79a039a30893c8c8f2f6fa4b40955a2477f00d51
Amar Raj <amarrajyekbote90@gmail.com>
2025-07-02 12:21:51 +0530

    only even in Simple.java file

---
+++ Simple.java
@@ -0,0 +1,10 @@
+class Simple{
+    public static void main(String args[]){
+        System.err.println("Printing Even Nums");
+        for(int i=0;i<20;i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
