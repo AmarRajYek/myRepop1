333d22a47969444b4b324b8262b5514e413f70b5
Amar Raj <amarrajyekbote90@gmail.com>
2025-07-02 12:34:40 +0530

    modified code adding methods for Even numbers & Added Odd also

--- Simple.java
+++ Simple.java
@@ -1,10 +1,27 @@
 class Simple{
-    public static void main(String args[]){
-        System.err.println("Printing Even Nums");
-        for(int i=0;i<20;i++){
-            if(i%2 == 0){
-                System.out.println(i);
+
+    void Odd(){
+        System.err.println("Printing Odd Nums");
+        for(int i=1;i<20;i++){
+            if(i%2!=0){
+                System.out.print(i+ "  ");
             }
         }
+        System.out.println();
+    }
+
+    void Even(){
+        System.err.println("Printing Even Nums");
+        for(int i=1;i<20;i++){
+            if(i%2==0){
+                System.out.print(i+ "  ");
+            }
+        }
+    }
+
+     public static void main(String args[]){
+        Simple sc  = new Simple();
+        sc.Odd();
+        sc.Even();
     }
 }
